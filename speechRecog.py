5581bba037d41c53367dbf0419772539782f823b
JawharBS <jawhar.bensalem@enis.tn>
2019-09-21 19:02:35 +0200

    Update speechRecog.py

--- speechRecog.py
+++ speechRecog.py
@@ -44,7 +44,7 @@ def index():
             with audio_file as source:
                 audio_data = recognizer.record(source)
             text = recognizer.recognize_google(
-                audio_data, key=GOOGLE_SPEECH_API_KEY, language="ru-RU"
+                audio_data, key=GOOGLE_SPEECH_API_KEY, language="en-US"
             )
             extra_line = f'Your text: "{text}"'
 
